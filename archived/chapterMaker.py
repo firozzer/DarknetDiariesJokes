86b479205f530d958cc2beddc038ee51f44c5ca2
Firoze <firozecool@gmail.com>
2022-10-01 21:05:05 +0400

    Timestamps working perfect

--- archived/chapterMaker.py
+++ archived/chapterMaker.py
@@ -1,6 +1,7 @@
-import re, pyperclip
-import datetime
-import os
+import re, pyperclip, datetime, os
+
+# ffmpeg code to create ffout.txt asf:
+# ffmpeg -i ytVidNew.mkv  -filter:v "select='gt(scene,0.2)',showinfo" -f null - 2> ffout.txt
 
 with open("ffout.txt") as f:
     data = f.read()
@@ -10,19 +11,18 @@ sRes = re.findall("pts_time:(.+)pos:", data)
 # making a dic of ep nos & names from the gathered jpgs
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 filenames = next(os.walk(os.path.dirname(os.path.abspath(__file__))), (None, None, []))[2]  # [] if no file
-epsDic = {}
+epNoAndNamesDic = {}
 for fname in filenames:
     if fname[-3:] == 'jpg': 
         fnameSplitted = fname[:-4].split()
-        epsDic[int(fnameSplitted[1])] = ' '.join(fnameSplitted[2:])
+        epNoAndNamesDic[int(fnameSplitted[1])] = ' '.join(fnameSplitted[2:])
 
-chaptersInDesc = "00:00 Ep 47 Project Raven\n"
+chaptersInDesc = "0:00:00 - Ep 47 Project Raven\n"
 
 for idx,res in enumerate(sRes):
     timeStamp = int(float(res.strip()))
     tsInHHMMSS = str(datetime.timedelta(seconds=timeStamp))
-    wholeLine = f"{tsInHHMMSS} - Ep {idx+48} {epsDic[idx+48]} \n"
+    wholeLine = f"{tsInHHMMSS} - Ep {idx+48} {epNoAndNamesDic[idx+48]} \n"
     chaptersInDesc += wholeLine
 
 pyperclip.copy(chaptersInDesc)
-
